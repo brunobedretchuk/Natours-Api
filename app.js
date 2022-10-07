b0fc0ab24e356ebcb5fafa8748a3a29a01bd7321
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-10-07 17:05:21 -0300

    get-requests

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 require('dotenv').config()
-
+const port = process.env.port;
 const app = express();
 
-app.get('/' , (req , res) => {
-    res.status(404)
-    .json({message : 'Hello from the server side!' , app : 'Natours'});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get('/api/v1/tours' , (req , res) => {
+       res.status(200)
+       .json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+       })
 })
 
-const port = process.env.port;
+
+
+
+
+// app.get('/' , (req , res) => {
+//     res.status(404)
+//     .json({message : 'Hello from the server side!' , app : 'Natours'});
+// })
+
 app.listen(port , () => {
     console.log(`App running on port ${port}`)
 });
