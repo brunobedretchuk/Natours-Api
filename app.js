6edea2198c55c85708db6bdbcfe06c1161a896f7
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2023-01-05 02:02:44 -0300

    first middleware

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req , res , next) => {
+  req.requestedAt = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
